2f59a801092395fb9e06bd03d342f0ee2e305931
raity <82751237+raityvojdani@users.noreply.github.com>
2024-06-03 21:58:47 +0330

    Add files via upload

---
+++ bubble-sort/bubble sort/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("1nter numbers:");
+int[] number = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+sort();
+
+
+void sort()
+{
+
+    for (int i = 0; i <= Math.Ceiling(Convert.ToDecimal(number.Length) / 2); i++)
+    {
+        for (int j = 0; j < number.Length - 1; j++)
+        {
+            if (number[j] > number[j + 1])
+            {
+                int temp = number[j];
+                number[j] = number[j + 1];
+                number[j + 1] = temp;
+            }
+        }
+    }
+    log();
+}
+void log()
+{
+    foreach (int num in number)
+    {
+        Console.WriteLine($"sorted:{num}");
+    }
+}
